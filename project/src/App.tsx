5a0e765beba3924773bdb08db2756c89417619f3
Alexandre Shyjada <alexandreshyjada@infojr.com.br>
2021-08-29 22:11:42 -0300

    add estado para darkmode no app e switchCheckbox no header para alterar o estado

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,12 +1,15 @@
+import {useState} from 'react'
 import { TaskList } from './components/TaskList'
 import { Header } from "./components/Header";
 import './styles/global.scss'
 
 
 export function App() {
+  const [ativo, setAtivo] = useState(false)
+
   return (
     <>
-      <Header />
+      <Header ativo={ativo} setAtivo={setAtivo}/>
       <TaskList />
     </>
   )
--- project/src/components/Header.tsx
+++ project/src/components/Header.tsx
@@ -1,11 +1,32 @@
 import '../styles/header.scss'
 
-export function Header() {
+interface IDarkMode {
+  ativo: boolean;
+  setAtivo: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export function Header(props: IDarkMode) {
+
+  const {ativo, setAtivo} = props
+
   return (
     <header className="header">
-      <div>
-        <img src="/logo.svg" alt="to.do"/>
+      <div className="containerHeader">
+        <div>
+          <img src="/logo.svg" alt="to.do"/>
+        </div>
+        <label className="switch">
+          <span className="textSwitch">
+            {!ativo ? "Ativar Dark Mode" : "Deativar Dark Mode"}
+          </span>
+          <div className="switchWrapper">
+            <input className="checkBox" type="checkbox" onChange={() => setAtivo(!ativo)}/>
+            <span className={!ativo? "switchButton light" : "switchButton dark"}></span>
+          </div>
+        </label>
       </div>
+
+
     </header>
   )
 }
